5b14ae1dde2c7f5e00fd93f76c4f19eefe23e8d4
MowMiKubek <83206616+MowMiKubek@users.noreply.github.com>
2023-02-13 23:12:22 +0100

    Bugfixes (surnames with -'-)

--- Simple_SCIHS/Student.cpp
+++ Simple_SCIHS/Student.cpp
@@ -40,6 +40,32 @@ Student::Student(std::string name, std::string surname, std::tuple<std::string, 
 
 }
 
+Student::Student(std::string name, std::string surname, std::tuple<std::string, std::string, std::string, std::string, std::string> address_street_house_nr_ap_nr_p_code_city, std::string pesel, std::string index, std::string email, std::string phone_num, std::vector<float> grades)
+{
+    name=Validator::rtrim(name);
+    surname=Validator::rtrim(surname);
+    phone_num = Validator::trim(phone_num);
+
+    try {
+        if(Validator::validate_name(name))
+            this->_name= name;
+        if(Validator::validate_surname(surname))
+            this->_surname = surname;
+        if(Validator::validate_pesel(pesel))
+            this->_pesel = pesel;
+        if(Validator::validate_index(index))
+            this->_index_nr = index;
+        if(Validator::validate_email(email))
+            this->_email = email;
+        if(Validator::validate_phone_number(phone_num))
+            this->_phone_number = phone_num;
+        this->_grades = grades;
+
+    }  catch (const char* msg) {
+        std::cerr << msg << std::endl;
+    }
+}
+
 void Student::show() const
 {
     std::cout<<this->index()<<" "<<this->name()<<" "<< this->surname()<<" "<<this->email()<<std::endl;
